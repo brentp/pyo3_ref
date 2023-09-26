255f4d7dd1da6468553c46a28b2c6375352e05fe
Brent Pedersen <bpederse@gmail.com>
2023-09-26 10:15:50 +0200

    working pyref?

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,54 @@
 use pyo3::prelude::*;
-use std::sync::{Arc, Mutex};
 
-#[derive(Debug)]
-pub struct Data {
-    x: [f64; 1000],
-}
-
-#[derive(Debug)]
-pub struct Outer {
-    _data: Data,
-}
-
-impl Outer {
-    pub fn data_mut(&mut self) -> &mut Data {
-        &mut self._data
-    }
-}
-
-#[derive(Debug)]
 #[pyclass]
-struct PyData {
-    // Q: what to put here for mutable access to &Data since we can't have lifetime.
-    outer: Arc<Mutex<Outer>>,
+struct Inner {
+    rust: Vec<i32>,
+}
+
+#[pyclass]
+struct Variant {
+    inner: Py<Inner>,
+}
+
+#[pyclass]
+struct Info {
+    variant: Py<Variant>,
 }
 
 #[pymethods]
-impl PyData {
-    fn set_data(&mut self, i: usize, x: f64, py: Python<'_>) -> PyResult<()> {
-        // ???
-        match self.outer.lock() {
-            Ok(mut outer) => {
-                outer.data_mut().x[i] = x;
-            }
-            Err(_) => {
-                return Err(PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
-                    "Failed to lock",
-                ))
-            }
+impl Variant {
+    #[new]
+    fn new(rust: Vec<i32>, py: Python<'_>) -> Self {
+        Self {
+            inner: Py::new(py, Inner { rust }).unwrap(),
         }
+    }
+
+    fn info(&mut self, py: Python<'_>) -> PyResult<Py<Info>> {
+        let variant = Py::new(
+            py,
+            Variant {
+                inner: self.inner.clone_ref(py),
+            },
+        )?;
+        let info = Py::new(py, Info { variant }).unwrap();
+        Ok(info)
+    }
+}
+
+#[pymethods]
+impl Info {
+    fn set_data(&mut self, index: usize, value: i32) -> PyResult<()> {
+        Python::with_gil(|py| {
+            let mut guard = self.variant.as_ref(py).borrow_mut();
+
+            let mutable = &mut *guard;
+            let mut iguard = mutable.inner.as_ref(py).borrow_mut();
+            let mutinner = &mut *iguard;
+            mutinner.rust[index] = value;
+        });
         Ok(())
     }
-
-    fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("{:?}", self.outer.lock().unwrap()._data.x))
-    }
-}
-
-#[pyclass]
-struct PyOuter {
-    rust: Arc<Mutex<Outer>>,
-}
-
-#[pymethods]
-impl PyOuter {
-    #[new]
-    fn new() -> Self {
-        Self {
-            rust: Arc::new(Mutex::new(Outer {
-                _data: Data { x: [0.0; 1000] },
-            })),
-        }
-    }
-
-    #[getter]
-    fn data(&mut self, py: Python<'_>) -> PyResult<PyData> {
-        Ok(PyData {
-            outer: self.rust.clone(),
-        })
-    }
-}
-
-#[pymodule]
-fn pyo3_ref(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<PyOuter>()?;
-    m.add_class::<PyData>()?;
-    Ok(())
 }
 
 #[cfg(test)]
@@ -83,14 +57,24 @@ mod tests {
 
     #[test]
     fn test_set_data() {
-        // create a PyOuter, access .data and set a value
         Python::with_gil(|py| {
-            let mut outer = PyOuter::new();
-            let mut data = outer.data(py).unwrap();
-            data.set_data(0, 1.0, py).unwrap();
+            // Create a new Variant
+            let rust_vec = vec![1, 2, 3];
+            let mut variant = Variant::new(rust_vec, py);
 
-            // check that the value was set
-            assert_eq!(outer.rust.lock().unwrap().data_mut().x[0], 1.0);
+            // Get the Info
+            let info = variant.info(py).unwrap();
+            let mut minfo = info.as_ref(py).borrow_mut();
+
+            // Set data
+            let index = 1;
+            let value = 5;
+            minfo.set_data(index, value).unwrap();
+
+            // Check the data
+            let mut guard = variant.inner.as_ref(py).borrow_mut();
+            let inner = &mut *guard;
+            assert_eq!(inner.rust[index], value);
         });
     }
 }

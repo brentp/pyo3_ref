246753009f6d1059bbaa0fdd8b735862bf3a7b88
Brent Pedersen <bpederse@gmail.com>
2023-10-03 09:24:03 +0200

    info setting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use noodles::vcf;
 use noodles::vcf::record::{info, position, Chromosome};
 use pyo3::prelude::*;
+use xvcf;
 
 #[pyclass]
 struct Variant {
@@ -8,8 +9,38 @@ struct Variant {
 }
 
 #[pyclass]
+struct VCF {
+    inner: xvcf::Reader,
+    header: vcf::Header,
+}
+
+#[pyclass]
 struct Info {
     variant: Py<Variant>,
+}
+
+#[pymethods]
+impl VCF {
+    #[new]
+    fn new(path: &str) -> PyResult<Self> {
+        let inner = xvcf::Reader::from_path(path).unwrap();
+        let header = inner.header().clone();
+        Ok(Self { inner, header })
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<Variant> {
+        let mut record = vcf::record::Record::default();
+        let header = slf.header.clone();
+        match slf.inner.next_record(&header, &mut record) {
+            Ok(0) => None,
+            Ok(_) => Some(Variant { inner: record }),
+            Err(_) => None,
+        }
+    }
 }
 
 #[pymethods]
@@ -122,10 +153,40 @@ impl InfoValue {
         Ok(format!("{:?}", self.0))
     }
 
+    #[staticmethod]
+    fn from_integer(i: i32) -> InfoValue {
+        InfoValue(info::field::Value::Integer(i))
+    }
+
+    #[staticmethod]
+    fn from_float(f: f32) -> InfoValue {
+        InfoValue(info::field::Value::Float(f))
+    }
+
+    #[staticmethod]
+    fn from_string(s: &str) -> InfoValue {
+        InfoValue(info::field::Value::String(s.to_string()))
+    }
+
     fn integer(&self) -> PyResult<i32> {
         match self.0 {
             info::field::Value::Integer(i) => Ok(i),
             _ => Err(pyo3::exceptions::PyTypeError::new_err("not an integer")),
+        }
+    }
+
+    fn float(&self) -> PyResult<f32> {
+        match self.0 {
+            info::field::Value::Float(f) => Ok(f),
+            info::field::Value::Integer(i) => Ok(i as f32),
+            _ => Err(pyo3::exceptions::PyTypeError::new_err("not a float")),
+        }
+    }
+
+    fn string(&self) -> PyResult<String> {
+        match &self.0 {
+            info::field::Value::String(s) => Ok(s.to_string()),
+            _ => Err(pyo3::exceptions::PyTypeError::new_err("not a string")),
         }
     }
 }
@@ -133,6 +194,11 @@ impl InfoValue {
 impl From<info::field::Value> for InfoValue {
     fn from(value: info::field::Value) -> Self {
         Self(value)
+    }
+}
+impl From<InfoValue> for info::field::Value {
+    fn from(value: InfoValue) -> Self {
+        value.0
     }
 }
 
@@ -153,6 +219,24 @@ impl Info {
                 Some(info) => Some(info.clone().into()),
             },
         }
+    }
+
+    fn set(&mut self, key: &str, value: &InfoValue, py: Python<'_>) -> PyResult<()> {
+        let mut guard = self.variant.as_ref(py).borrow_mut();
+        let variant = &mut *guard;
+
+        let o: info::field::Key = match key.parse() {
+            Ok(key) => key,
+            Err(_) => {
+                return Err(pyo3::exceptions::PyKeyError::new_err(format!(
+                    "invalid key: {}",
+                    key
+                )))
+            }
+        };
+        let v: info::field::Value = value.0.clone();
+        variant.inner.info_mut().insert(o, Some(v));
+        Ok(())
     }
 
     fn __repr__(&self, py: Python<'_>) -> PyResult<String> {
@@ -192,15 +276,34 @@ mod tests {
                 print(v)
                 print(v.info.get('DP'))
                 assert v.info.get('DP').integer() == 10
-                #assert v.chromosome == 'chrX'
-                #v.chromosome = 'chr22'; 
-                #assert v.chromosome == 'chr22'
-                #info = v.info
-                #assert v.chromosome == 'chr22'
-                #print(info)
 
-                #print(info.get('DP'))
+                v.info.set('DP', InfoValue.from_integer(99))
+                assert v.info.get('DP').integer() == 99
+
+                v.info.set('DP', InfoValue.from_string("hello"))
+                print(v.info.get('DP'))
                 
+
+                
+                "#
+            );
+        });
+    }
+
+    #[test]
+    fn test_read_file() {
+        Python::with_gil(|py| {
+            // write a test to check the chromosome
+            let locals = [("VCF", py.get_type::<VCF>())].into_py_dict(py);
+
+            pyo3::py_run!(
+                py,
+                *locals,
+                r#"
+                vcf = VCF('tests/t.vcf.gz')
+                for variant in vcf:
+                    print(variant.info.get('DP'))
+                    print(variant.info.get('DP').integer())
                 "#
             );
         });

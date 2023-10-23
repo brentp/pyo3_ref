c634394ce87586b2a43e7f2b23a0cb88d9751da0
Brent Pedersen <bpederse@gmail.com>
2023-10-23 14:06:54 +0200

    another try at lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,22 @@ use noodles::vcf;
 use noodles_util::variant;
 use std::io;
 
-struct QueryHolder /* <'a: 'h> */ {
-    vcf: variant::indexed_reader::IndexedReader<std::fs::File>,
+struct QueryHolder<'r, 'h: 'r> {
     query: Box<dyn Iterator<Item = io::Result<vcf::Record>>>,
-    // header: &'h vcf::Header,
+    header: &'h vcf::Header,
+    reader: &'r variant::indexed_reader::IndexedReader<std::fs::File>,
 }
 
-fn get_qh(vcf_path: String, region: &Region) -> Result<QueryHolder, Box<dyn std::error::Error>> {
-    let mut reader = variant::indexed_reader::Builder::default().build_from_path(vcf_path)?;
-    let header = reader.read_header()?;
-
+fn get_qh<'h, 'r: 'h>(
+    reader: &'r mut variant::IndexedReader<std::fs::File>,
+    header: &'h vcf::Header,
+    region: &Region,
+) -> Result<QueryHolder<'r, 'h>, Box<dyn std::error::Error>> {
     let q = reader.query(&header, &region)?;
     let qh = QueryHolder {
-        vcf: reader,
         query: Box::new(q),
+        header: header,
+        reader,
     };
     Ok(qh)
 }
@@ -24,10 +26,12 @@ fn get_qh(vcf_path: String, region: &Region) -> Result<QueryHolder, Box<dyn std:
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args().skip(1);
     let vcf_path = args.next().expect("missing vcf path");
+    let mut reader = variant::indexed_reader::Builder::default().build_from_path(vcf_path)?;
+    let header = reader.read_header()?;
 
     let region = "chr1".parse()?;
 
-    let qh = get_qh(vcf_path, &region)?;
+    let qh = get_qh(&mut reader, &header, &region)?;
 
     Ok(())
 }

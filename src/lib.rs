d0c27da031e0589ac8f683b3750f22e13fab211b
Brent Pedersen <bpederse@gmail.com>
2023-10-23 13:51:02 +0200

    noodles query example

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,33 @@
-use mlua::*;
-use parking_lot::Mutex;
+use noodles::core::Region;
+use noodles::vcf;
+use noodles_util::variant;
 use std::io;
-use std::sync::Arc;
 
-//use std::rc::Rc as Arc; // must be send.
-
-pub struct Inner {
-    i: i32,
-    j: i32,
+struct QueryHolder /* <'a: 'h> */ {
+    vcf: variant::indexed_reader::IndexedReader<std::fs::File>,
+    query: Box<dyn Iterator<Item = io::Result<vcf::Record>>>,
+    // header: &'h vcf::Header,
 }
 
-pub struct Outer {
-    inner: Arc<Mutex<Inner>>,
-    a: i32,
-    b: i32,
+fn get_qh(vcf_path: String, region: &Region) -> Result<QueryHolder, Box<dyn std::error::Error>> {
+    let mut reader = variant::indexed_reader::Builder::default().build_from_path(vcf_path)?;
+    let header = reader.read_header()?;
+
+    let q = reader.query(&header, &region)?;
+    let qh = QueryHolder {
+        vcf: reader,
+        query: Box::new(q),
+    };
+    Ok(qh)
 }
 
-//impl UserData for Inner {}
-impl UserData for Outer {} // it seems this is required.
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args().skip(1);
+    let vcf_path = args.next().expect("missing vcf path");
 
-pub fn main() -> io::Result<()> {
-    let lua = Lua::new();
+    let region = "chr1".parse()?;
 
-    lua.register_userdata_type::<Arc<Mutex<Inner>>>(|lp| {
-        lp.add_field_method_get("i", |_, this| Ok(this.lock().i));
-        lp.add_field_method_get("j", |_, this| Ok(this.lock().j));
-    })
-    .unwrap();
-
-    lua.register_userdata_type::<Outer>(|lp| {
-        lp.add_field_method_get("a", |_, this| Ok(this.a));
-        lp.add_field_method_get("b", |_, this| Ok(this.b));
-        lp.add_field_method_get("inner", |lua, this| {
-            lua.create_any_userdata(this.inner.clone())
-        });
-    })
-    .unwrap();
-
-    // make an example constructor and data. to test.
-    let outer_constructor = lua
-        .create_function(|_lua, (a, b): (i32, i32)| {
-            Ok(Outer {
-                inner: Arc::new(Mutex::new(Inner { i: a, j: b })),
-                a: 2 * a,
-                b: 2 * b,
-            })
-        })
-        .unwrap();
-    lua.globals().set("Outer", outer_constructor).unwrap();
-
-    let code = r#"
-        local outer = Outer(1, 2)
-        print(outer.a, outer.b)
-        print(outer.inner.i, outer.inner.j)
-        "#;
-    lua.load(code).exec().unwrap();
+    let qh = get_qh(vcf_path, &region)?;
 
     Ok(())
 }

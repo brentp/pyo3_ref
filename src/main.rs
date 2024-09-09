36736f71d54ba8c870bef47b43407b61dd38950e
brent <bpederse@gmail.com>
2024-09-09 14:29:25 -0700

    leak

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use std::sync::Arc;
 
 use rust_htslib::bcf::{self, Record};
 use rust_htslib::bcf::record::GenotypeAllele;
-use rusty_v8 as v8;
+use v8;
+
+// see: https://github.com/denoland/deno/blob/41cad2179fb36c2371ab84ce587d3460af64b5fb/ext/napi/lib.rs#L522-L527
 
 struct Variant {
     record: rust_htslib::bcf::Record,
 }
+
 
 impl Variant {
     fn new(record: rust_htslib::bcf::Record) -> Self {
@@ -18,7 +21,7 @@ impl Variant {
     }
 }
 
-fn create_vcf_record() -> Result<Record, Box<dyn std::error::Error>> {
+fn create_header() -> bcf::Header {
     let mut header = bcf::Header::new();
     header.push_record(r#"##contig=<ID=chr1,length=10000>"#.as_bytes());
     header.push_record(
@@ -30,7 +33,10 @@ fn create_vcf_record() -> Result<Record, Box<dyn std::error::Error>> {
     );
     header.push_sample("NA12878".as_bytes());
     header.push_sample("NA12879".as_bytes());
-    let vcf = bcf::Writer::from_path("_test.vcf", &header, true, bcf::Format::Vcf).unwrap();
+    header
+}
+
+fn create_vcf_record(_header: &bcf::Header, vcf: &bcf::Writer) -> Result<Record, Box<dyn std::error::Error>> {
     let mut record = vcf.empty_record();
     let _ = record.set_rid(Some(vcf.header().name2rid(b"chr1").unwrap()));
     record.set_pos(6);
@@ -48,35 +54,77 @@ fn create_vcf_record() -> Result<Record, Box<dyn std::error::Error>> {
     Ok(record)
 }
 
+impl Drop for Variant {
+    fn drop(&mut self) {
+        eprintln!("Dropping variant");
+        // Cleanup code if necessary
+    }
+}
+
+fn start_getter(
+    scope: &mut v8::HandleScope,
+    _key: v8::Local<v8::Name>,
+    args: v8::PropertyCallbackArguments,
+    mut rv: v8::ReturnValue,
+) {
+    let this = args.this();
+    //eprintln!("key: {:?}", key.to_rust_string_lossy(scope));
+    let internal_field = this.get_internal_field(scope, 0).unwrap();
+    let external = v8::Local::<v8::External>::try_from(internal_field).unwrap();
+    let variant_ptr = external.value() as *const Variant;
+    let variant = unsafe { &*variant_ptr };
+
+    rv.set(v8::Number::new(scope, variant.start() as f64).into());
+}
+
+fn weak_callback(
+    isolate: &mut v8::Isolate,
+    weak: v8::Weak<v8::External>,
+    _parameter: *mut std::ffi::c_void,
+) {
+    // Recover the Arc<Variant> from the raw pointer and drop it
+    let scope = &mut v8::HandleScope::new(isolate);
+    let external = weak.to_local(scope).expect("error getting to_local");
+    let variant_ptr = external.value() as *const Variant;
+    unsafe {
+        // Convert back to Arc and drop it, freeing memory if this is the last reference
+        Arc::from_raw(variant_ptr);
+    }
+}
+
 fn create_variant_object<'a>(
     scope: &mut v8::HandleScope<'a>,
     variant: Arc<Variant>,
-) -> v8::Local<'a, v8::Object> {
+) -> (v8::Local<'a, v8::Object>, v8::Weak<v8::Object>) {
     let object_template = v8::ObjectTemplate::new(scope);
     object_template.set_internal_field_count(1);
 
-    // Fix the start_getter function
     let start_name = v8::String::new(scope, "start").unwrap();
-    let start_getter = |scope: &mut v8::HandleScope,
-                        _property: v8::Local<v8::Name>,
-                        args: v8::PropertyCallbackArguments,
-                        mut retval: v8::ReturnValue| {
-        let this = args.this();
-        let external = unsafe { v8::Local::<v8::External>::cast(this.get_internal_field(scope, 0).unwrap()) };
-        let variant_ptr = external.value() as *const Variant;
-        let variant = unsafe { &*variant_ptr };
-
-        let start = variant.start();
-        let start_value = v8::Integer::new(scope, start as i32);
-        retval.set(start_value.into());
-    };
     object_template.set_accessor(start_name.into(), start_getter);
 
     let object = object_template.new_instance(scope).unwrap();
-    let external_variant = v8::External::new(scope, Arc::into_raw(variant) as *mut _);
-    object.set_internal_field(0, external_variant.into());
 
-    object
+    // NOTE this will leak unless we call Arc::from_raw 
+    let variant_ptr = Arc::into_raw(variant);
+
+    let external_variant = v8::External::new(scope, variant_ptr as *mut _);
+    object.set_internal_field(0, external_variant.into());
+    // TODO: copy what's done here: https://github.com/denoland/deno/blob/41cad2179fb36c2371ab84ce587d3460af64b5fb/ext/napi/lib.rs#L522-L527 ?
+
+    let weak = v8::Weak::with_guaranteed_finalizer(
+        scope,
+        object,
+        Box::new(move || {
+            eprintln!("Finalizing weak reference");
+            unsafe { Arc::from_raw(variant_ptr); }
+        }),
+    );
+
+    (object, weak)
+}
+
+fn trigger_gc(isolate: &mut v8::Isolate) {
+    isolate.request_garbage_collection_for_testing(v8::GarbageCollectionType::Full);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -85,26 +133,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 
-    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+    let isolate = &mut v8::Isolate::new(Default::default());
     let handle_scope = &mut v8::HandleScope::new(isolate);
-    let context = v8::Context::new(handle_scope);
+    let context = v8::Context::new(handle_scope, Default::default());
     let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-    let record = create_vcf_record()?;
-    let variant = Arc::new(Variant::new(record));
-    let variant_object = create_variant_object(scope, variant);
+    // Create header and VCF record
+    let header = create_header();
+    let vcf = bcf::Writer::from_path("_test.vcf", &header, true, bcf::Format::Vcf).unwrap();
+    let mut weak_refs : Vec<v8::Weak<v8::Object>> = Vec::new();
+    let n = 1000000;
+    for _i in 0..n {
+        let record = create_vcf_record(&header, &vcf)?;
+        let variant = Arc::new(Variant::new(record));
 
-    // Add the variant object to the global object
-    let global = context.global(scope);
-    let variant_name = v8::String::new(scope, "variant").unwrap();
-    global.set(scope, variant_name.into(), variant_object.into());
+        // Create the variant object in V8
+        let (variant_object, weak) = create_variant_object(scope, variant.clone());
+        weak_refs.push(weak);
 
-    // Execute JavaScript code
-    let source = v8::String::new(scope, "variant.start").unwrap();
-    let script = v8::Script::compile(scope, source, None).unwrap();
-    let result = script.run(scope).unwrap();
+        // Set the variant object in the global context
+        let global = context.global(scope);
+        let variant_name = v8::String::new(scope, "variant").unwrap();
+        global.set(scope, variant_name.into(), variant_object.into());
 
-    println!("Result: {:?}", result.to_string(scope).unwrap().to_rust_string_lossy(scope));
+        // Run the JavaScript code
+        let code = v8::String::new(scope, "variant.start").unwrap();
+        let script = v8::Script::compile(scope, code, None).unwrap();
+        let result = script.run(scope).unwrap();
+
+        // Convert the result to a string and print it
+        let result_str = result.to_string(scope).unwrap();
+        println!("variant.start: {}, i: {}/{}", result_str.to_rust_string_lossy(scope), _i, n);
+    }
+    unsafe {
+        v8::V8::dispose();
+        v8::V8::dispose_platform();
+    }
+    eprintln!("done");
+    // sleep for 100 seconds
+    std::thread::sleep(std::time::Duration::from_secs(20));
+
 
     Ok(())
 }

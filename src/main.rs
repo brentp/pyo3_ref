d24d363c2102d85ee563d371fce814e277fd8acc
brent <bpederse@gmail.com>
2024-09-11 08:00:00 -0700

    try using cppgc

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ struct Variant {
     _make_this_use_memory: [u64; 128]
 }
 
-
 impl Variant {
     fn new(chrom: String, start: i32, end: i32) -> Self {
         Self { _chrom: chrom, _start: start, _end: end, _make_this_use_memory: [0; 128] }
@@ -34,6 +33,18 @@ impl Drop for Variant {
     }
 }
 
+struct VariantWrapper {
+    variant: Arc<Variant>,
+}
+
+impl v8::cppgc::GarbageCollected for VariantWrapper {
+    /* 
+    fn trace(&self, _visitor: &v8::cppgc::Visitor) {
+        // No need to trace Arc<Variant> as it doesn't contain any GC references
+    }
+    */
+}
+
 fn attr_getter(
     scope: &mut v8::HandleScope,
     key: v8::Local<v8::Name>,
@@ -41,11 +52,14 @@ fn attr_getter(
     mut rv: v8::ReturnValue,
 ) {
     let this = args.this();
-    //eprintln!("key: {:?}", key.to_rust_string_lossy(scope));
-    let internal_field = this.get_internal_field(scope, 0).unwrap();
-    let external = v8::Local::<v8::External>::try_from(internal_field).unwrap();
-    let variant_ptr = external.value() as *const Variant;
-    let variant = unsafe { &*variant_ptr };
+    let wrapper = v8::Local::<v8::Object>::try_from(this)
+        .ok()
+        .and_then(|obj| obj.get_internal_field(scope, 0))
+        .and_then(|field| v8::Local::<v8::External>::try_from(field).ok())
+        .and_then(|ext| Some(ext.value() as *mut VariantWrapper))
+        .expect("Failed to get VariantWrapper");
+
+    let variant = unsafe { &(*wrapper).variant };
 
     match key.to_rust_string_lossy(scope).as_bytes() {
         b"start" => {
@@ -67,28 +81,19 @@ fn attr_getter(
             rv.set(error.into());
         }
     }
-
 }
 
-fn weak_callback(
-    isolate: &mut v8::Isolate,
-    weak: v8::Weak<v8::External>,
-    _parameter: *mut std::ffi::c_void,
-) {
-    // Recover the Arc<Variant> from the raw pointer and drop it
-    let scope = &mut v8::HandleScope::new(isolate);
-    let external = weak.to_local(scope).expect("error getting to_local");
-    let variant_ptr = external.value() as *const Variant;
-    unsafe {
-        // Convert back to Arc and drop it, freeing memory if this is the last reference
-        Arc::from_raw(variant_ptr);
-    }
-}
+const TAG: u16 = 1;
 
 fn create_variant_object<'a>(
     scope: &mut v8::HandleScope<'a>,
     variant: Arc<Variant>,
-) -> (v8::Local<'a, v8::Object>, v8::Weak<v8::Object>) {
+) -> v8::Local<'a, v8::Object> {
+    let wrapper = unsafe { v8::cppgc::make_garbage_collected::<VariantWrapper>(
+        scope.get_cpp_heap().unwrap(),
+        VariantWrapper { variant },
+    ) };
+
     let object_template = v8::ObjectTemplate::new(scope);
     object_template.set_internal_field_count(1);
 
@@ -100,34 +105,28 @@ fn create_variant_object<'a>(
     object_template.set_accessor(chrom_name.into(), attr_getter);
 
     let object = object_template.new_instance(scope).unwrap();
+    unsafe {
+        v8::Object::wrap::<TAG, VariantWrapper>(scope, object, &wrapper);
+    }
 
-    // NOTE this will leak unless we call Arc::from_raw 
-    let variant_ptr = Arc::into_raw(variant);
-
-    let external_variant = v8::External::new(scope, variant_ptr as *mut _);
-    object.set_internal_field(0, external_variant.into());
-    // TODO: copy what's done here: https://github.com/denoland/deno/blob/41cad2179fb36c2371ab84ce587d3460af64b5fb/ext/napi/lib.rs#L522-L527 ?
-
-    let weak = v8::Weak::with_guaranteed_finalizer(
-        scope,
-        object,
-        Box::new(move || {
-            eprintln!("Finalizing weak reference");
-            unsafe { Arc::from_raw(variant_ptr); }
-        }),
-    );
-
-    (object, weak)
+    object
 }
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize V8
+    // Initialize V8 with cppgc
     let platform = v8::new_default_platform(0, false).make_shared();
-    v8::V8::initialize_platform(platform);
+    v8::V8::initialize_platform(platform.clone());
     v8::V8::initialize();
 
-    let isolate = &mut v8::Isolate::new(Default::default());
+    v8::cppgc::initalize_process(platform.clone());
+
+
+    let heap =
+    v8::cppgc::Heap::create(platform, v8::cppgc::HeapCreateParams::default());
+
+
+    let isolate = &mut v8::Isolate::new(v8::CreateParams::default().cpp_heap(heap));
+
     let handle_scope = &mut v8::HandleScope::new(isolate);
     let context = v8::Context::new(handle_scope, Default::default());
     let scope = &mut v8::ContextScope::new(handle_scope, context);
@@ -140,7 +139,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let variant = Arc::new(record);
 
         // Create the variant object in V8
-        let (variant_object, weak) = create_variant_object(scope, variant.clone());
+        let variant_object = create_variant_object(scope, variant.clone());
 
         //weak_refs.push(weak);
 
@@ -153,11 +152,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let code = v8::String::new(scope, "variant.start").unwrap();
         let script = v8::Script::compile(scope, code, None).unwrap();
         let result = script.run(scope).unwrap();
+        global.delete(scope, variant_name.into());
 
         // Convert the result to a string and print it
         let result_str = result.to_string(scope).unwrap();
         println!("variant.start: {}, /{}", result_str.to_rust_string_lossy(scope), n);
     }
+
+    // Perform garbage collection
+    scope.request_garbage_collection_for_testing(v8::GarbageCollectionType::Full);
+
     unsafe {
         v8::V8::dispose();
         v8::V8::dispose_platform();
@@ -165,7 +169,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     eprintln!("done");
     // sleep for 100 seconds
     std::thread::sleep(std::time::Duration::from_secs(20));
-
 
     Ok(())
 }
